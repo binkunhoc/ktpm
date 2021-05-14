2b6d55682b79bd87a5bd096d76dac65c64f308f7
binkunhoc <50960371+binkunhoc@users.noreply.github.com>
2021-05-14 22:34:16 +0700

    Delete ktpm-main directory

--- ktpm-main/Assignment 9/bai3.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-import time
-
-chrome_driver_path = "C:\ChromeDriver\chromedriver.exe"
-driver = webdriver.Chrome(chrome_driver_path)
-driver.get("http://practice.automationtesting.in/")
-driver.set_window_size(1500, 1000)
-time.sleep(3)
-driver.close()
